7fcbc568cf9cb022c60aa4958f3b87872e308335
sweetpea22 <cindychau24@me.com>
2020-08-06 14:47:35 -0400

    upgrade yarn

---
+++ web/lib/geodata.js
@@ -0,0 +1,19 @@
+const geo = [
+  {
+    peerId: "Wj2LgQXymTopKUff1fmM2QJ6MtAfk1x94NNLqSeD",
+    client: { type: "Lotus", version: "0.2.4" },
+    location: { Lat: 32.8312, Lon: -117.1225 },
+    city: "San Diego",
+    country: "United States",
+    ISP: "Hetzner",
+  },
+
+  {
+    peerId: "Wj2LgQXymTopKUff1fmM2QJ6MtAfk1x94NNLqSeD",
+    client: { type: "Lotus", version: "0.2.4" },
+    location: { Lat: -25.42, Lon: -49.32 },
+    city: "Curitiba",
+    country: "Brazil",
+    ISP: "Hetzner",
+  },
+];
